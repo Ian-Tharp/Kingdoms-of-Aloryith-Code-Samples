01a006298213ad9d04e610df8d59f4a0302f3812
Ian Tharp <64619437+Ian-Tharp@users.noreply.github.com>
2021-08-30 15:32:45 -0500

    Attributes component

---
+++ AC_PlayerAttributes.cpp
@@ -0,0 +1,208 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AC_PlayerAttributes.h"
+
+// Sets default values for this component's properties
+UAC_PlayerAttributes::UAC_PlayerAttributes()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	//SetIsReplicatedByDefault(true);
+}
+
+// Called when the game starts
+void UAC_PlayerAttributes::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UAC_PlayerAttributes::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+//Calculating how much damage the player should take
+void UAC_PlayerAttributes::CalculateIncomingDamage(float Amount) {
+	
+}
+
+
+
+
+//Adding Main Player Attributes
+void UAC_PlayerAttributes::AddStrength(int32 Amount) {
+	Strength += Amount;
+	MeleeAttackPower += Amount;
+	MeleeHitChance += Amount * 0.1f;
+	MeleeCriticalChance += Amount * 0.01f;
+	ArmorPenetration += Amount * 0.01f;
+	ParryChance += Amount / 200.0f;
+	BlockChance += Amount / 200.0f;
+}
+void UAC_PlayerAttributes::AddAgility(int32 Amount) {
+	Agility += Amount;
+	MeleeAttackPower += Amount / 2;
+	MeleeHitChance += Amount * 0.03f;
+	MeleeCriticalChance += Amount * 0.01f;
+	RangedAttackPower += Amount;
+	RangedHitChance += Amount * 0.3f;
+	RangedCriticalChance += Amount * 0.01f;
+	DodgeChance += Amount / 200.0f;
+	Haste += Amount / 250.0f;
+}
+void UAC_PlayerAttributes::AddEndurance(int32 Amount) {
+	Endurance += Amount;
+	CurrentHealth += Amount * 10;
+	MaxHealth += Amount * 10;
+	Armor += Amount * 2;
+}
+void UAC_PlayerAttributes::AddIntellect(int32 Amount) {
+	Intellect += Amount;
+	SpellPower += Amount;
+	SpellHitChance += Amount * 0.1f;
+	SpellCriticalChance += Amount * 0.01f;
+	SpellPenetration += Amount * 0.01f;
+	Proficiency += Amount;
+}
+void UAC_PlayerAttributes::AddWisdom(int32 Amount) {
+	Wisdom += Amount;
+	SpellPower += Amount / 2;
+	SpellHitChance += Amount * 0.05f;
+	SpellCriticalChance += Amount * 0.01f;
+	Haste += Amount / 100.0f;
+}
+void UAC_PlayerAttributes::AddCharisma(int32 Amount) {
+	Charisma += Amount;
+	Devotion += Amount * 2;
+}
+void UAC_PlayerAttributes::AddArmor(int32 Amount) { Armor += Amount; }
+void UAC_PlayerAttributes::AddAllMainStats(int32 Amount) {
+	AddStrength(Amount);
+	AddAgility(Amount);
+	AddEndurance(Amount);
+	AddIntellect(Amount);
+	AddWisdom(Amount);
+	AddCharisma(Amount);
+}
+
+//Subtracting Main Player Attributes
+void UAC_PlayerAttributes::SubtractStrength(int32 Amount) {
+	Strength -= Amount;
+	MeleeAttackPower -= Amount;
+	MeleeHitChance -= Amount * 0.1f;
+	MeleeCriticalChance -= Amount * 0.01f;
+	ParryChance -= Amount / 200.0f;
+	BlockChance -= Amount / 200.0f;
+}
+void UAC_PlayerAttributes::SubtractAgility(int32 Amount) {
+	Agility -= Amount;
+	MeleeAttackPower -= Amount / 2;
+	MeleeHitChance -= Amount * 0.05f;
+	MeleeCriticalChance -= Amount * 0.01f;
+	RangedAttackPower -= Amount;
+	RangedHitChance -= Amount * 0.1f;
+	RangedCriticalChance -= Amount * 0.01f;
+	DodgeChance -= Amount / 200.0f;
+	Haste -= Amount / 250.0f;
+}
+void UAC_PlayerAttributes::SubtractEndurance(int32 Amount) {
+	Endurance -= Amount;
+	CurrentHealth -= Amount * 10;
+	MaxHealth -= Amount * 10;
+	Armor -= Amount * 2;
+}
+void UAC_PlayerAttributes::SubtractIntellect(int32 Amount) {
+	Intellect -= Amount;
+	SpellPower -= Amount;
+	SpellHitChance -= Amount * 0.1f;
+	SpellCriticalChance -= Amount * 0.01f;
+	SpellPenetration -= Amount * 0.01f;
+	Proficiency -= Amount;
+}
+void UAC_PlayerAttributes::SubtractWisdom(int32 Amount) {
+	Wisdom -= Amount;
+	SpellPower -= Amount / 2;
+	SpellHitChance -= Amount * 0.05f;
+	SpellCriticalChance -= Amount * 0.01f;
+	Haste -= Amount / 100.0f;
+}
+void UAC_PlayerAttributes::SubtractCharisma(int32 Amount) {
+	Charisma -= Amount;
+	Devotion -= Amount * 2;
+}
+void UAC_PlayerAttributes::SubtractArmor(int32 Amount) { Armor -= Amount; }
+void UAC_PlayerAttributes::SubtractAllMainStats(int32 Amount) {
+	SubtractStrength(Amount);
+	SubtractAgility(Amount);
+	SubtractEndurance(Amount);
+	SubtractIntellect(Amount);
+	SubtractWisdom(Amount);
+	SubtractCharisma(Amount);
+}
+
+//Adding Combat Attributes
+void UAC_PlayerAttributes::AddMeleeAttackPower(int32 Amount) { MeleeAttackPower += Amount; }
+void UAC_PlayerAttributes::AddRangedAttackPower(int32 Amount) { RangedAttackPower += Amount; }
+void UAC_PlayerAttributes::AddSpellPower(int32 Amount) { SpellPower += Amount; }
+void UAC_PlayerAttributes::AddMeleeHitChance(float Amount) { MeleeHitChance += Amount; }
+void UAC_PlayerAttributes::AddRangedHitChance(float Amount) { RangedHitChance += Amount; }
+void UAC_PlayerAttributes::AddSpellHitChance(float Amount) { SpellHitChance += Amount; }
+void UAC_PlayerAttributes::AddMeleeCriticalChance(float Amount) { MeleeCriticalChance += Amount; }
+void UAC_PlayerAttributes::AddRangedCriticalChance(float Amount) { RangedCriticalChance += Amount; }
+void UAC_PlayerAttributes::AddSpellCriticalChance(float Amount) { SpellCriticalChance += Amount; }
+void UAC_PlayerAttributes::AddArmorPenetration(float Amount) { ArmorPenetration += Amount; }
+void UAC_PlayerAttributes::AddSpellPenetration(float Amount) { SpellPenetration += Amount; }
+void UAC_PlayerAttributes::AddDodgeChance(float Amount) { DodgeChance += Amount; }
+void UAC_PlayerAttributes::AddParryChance(float Amount) { ParryChance += Amount; }
+void UAC_PlayerAttributes::AddBlockChance(float Amount) { BlockChance += Amount; }
+void UAC_PlayerAttributes::AddBlockAmount(float Amount) { BlockAmount += Amount; }
+void UAC_PlayerAttributes::AddHaste(float Amount) { Haste += Amount; }
+//Subtracting Combat Attributes
+void UAC_PlayerAttributes::SubtractMeleeAttackPower(int32 Amount) { MeleeAttackPower -= Amount; }
+void UAC_PlayerAttributes::SubtractRangedAttackPower(int32 Amount) { RangedAttackPower -= Amount; }
+void UAC_PlayerAttributes::SubtractSpellPower(int32 Amount) { SpellPower -= Amount; }
+void UAC_PlayerAttributes::SubtractMeleeHitChance(float Amount) { MeleeHitChance -= Amount; }
+void UAC_PlayerAttributes::SubtractRangedHitChance(float Amount) { RangedHitChance -= Amount; }
+void UAC_PlayerAttributes::SubtractSpellHitChance(float Amount) { SpellHitChance -= Amount; }
+void UAC_PlayerAttributes::SubtractMeleeCriticalChance(float Amount) { MeleeCriticalChance -= Amount; }
+void UAC_PlayerAttributes::SubtractRangedCriticalChance(float Amount) { RangedCriticalChance -= Amount; }
+void UAC_PlayerAttributes::SubtractSpellCriticalChance(float Amount) { SpellCriticalChance -= Amount; }
+void UAC_PlayerAttributes::SubtractArmorPenetration(float Amount) { ArmorPenetration -= Amount; }
+void UAC_PlayerAttributes::SubtractSpellPenetration(float Amount) { SpellPenetration -= Amount; }
+void UAC_PlayerAttributes::SubtractDodgeChance(float Amount) { DodgeChance -= Amount; }
+void UAC_PlayerAttributes::SubtractParryChance(float Amount) { ParryChance -= Amount; }
+void UAC_PlayerAttributes::SubtractBlockChance(float Amount) { BlockChance -= Amount; }
+void UAC_PlayerAttributes::SubtractBlockAmount(float Amount) { BlockAmount -= Amount; }
+void UAC_PlayerAttributes::SubtractHaste(float Amount) { Haste -= Amount; }
+//Adding Resistances
+void UAC_PlayerAttributes::AddFireResistance(int32 Amount) { FireResistance += Amount; }
+void UAC_PlayerAttributes::AddFrostResistance(int32 Amount) { FrostResistance += Amount; }
+void UAC_PlayerAttributes::AddHolyResistance(int32 Amount) { HolyResistance += Amount; }
+void UAC_PlayerAttributes::AddShadowResistance(int32 Amount) { ShadowResistance += Amount; }
+void UAC_PlayerAttributes::AddNatureResistance(int32 Amount) { NatureResistance += Amount; }
+void UAC_PlayerAttributes::AddArcaneResistance(int32 Amount) { ArcaneResistance += Amount; }
+//Subtracting Resistances
+void UAC_PlayerAttributes::SubtractFireResistance(int32 Amount) { FireResistance -= Amount; }
+void UAC_PlayerAttributes::SubtractFrostResistance(int32 Amount) { FrostResistance -= Amount; }
+void UAC_PlayerAttributes::SubtractHolyResistance(int32 Amount) { HolyResistance -= Amount; }
+void UAC_PlayerAttributes::SubtractShadowResistance(int32 Amount) { ShadowResistance -= Amount; }
+void UAC_PlayerAttributes::SubtractNatureResistance(int32 Amount) { NatureResistance -= Amount; }
+void UAC_PlayerAttributes::SubtractArcaneResistance(int32 Amount) { ArcaneResistance -= Amount; }
+
+//Adding/Subtracting Misc Attributes
+void UAC_PlayerAttributes::AddDevotion(int32 Amount) { Devotion += Amount; }
+void UAC_PlayerAttributes::AddProficiency(int32 Amount) { Proficiency += Amount; }
+void UAC_PlayerAttributes::SubtractDevotion(int32 Amount) { Devotion -= Amount; }
+void UAC_PlayerAttributes::SubtractProficiency(int32 Amount) { Proficiency -= Amount; }
+
---
+++ AC_PlayerAttributes.h
@@ -0,0 +1,245 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AC_PlayerAttributes.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CODENAME_COBALT_API UAC_PlayerAttributes : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this component's properties
+	UAC_PlayerAttributes();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+	//Main Attribute Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 0))
+		int32 CurrentHealth = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 0))
+		int32 MaxHealth = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 0))
+		int32 CurrentResource = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 0))
+		int32 MaxResource = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Strength = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Agility = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Endurance = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Intellect = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Wisdom = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 1))
+		int32 Charisma = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Main Stats", meta = (ClampMin = 0))
+		int32 Armor = 1;
+	
+	//Combat and Calculation Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		int32 MeleeAttackPower = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		int32 RangedAttackPower = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		int32 SpellPower = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float MeleeHitChance = 75.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float RangedHitChance = 75.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float SpellHitChance = 75.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float MeleeCriticalChance = 8.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float RangedCriticalChance = 8.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float SpellCriticalChance = 8.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float ArmorPenetration = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float SpellPenetration = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float DodgeChance = 7.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float ParryChance = 7.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float BlockChance = 7.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		int BlockAmount = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat Stats", meta = (ClampMin = 0))
+		float Haste = 0.0f;
+	//Resistances
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 FireResistance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 FrostResistance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 HolyResistance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 ShadowResistance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 NatureResistance = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resistances")
+		int32 ArcaneResistance = 0;
+	//Miscellaneous Attribute Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Misc Stats", meta = (ClampMin = 0))
+		int32 Devotion = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Misc Stats", meta = (ClampMin = 0))
+		int32 Proficiency = 0;
+
+
+	//Functions to Add/Subtract Amounts to Main Attributes
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddStrength(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddAgility(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddEndurance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddIntellect(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddWisdom(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddCharisma(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddArmor(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void AddAllMainStats(int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractStrength(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractAgility(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractEndurance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractIntellect(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractWisdom(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractCharisma(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractArmor(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Main Stats")
+		void SubtractAllMainStats(int32 Amount);
+
+	//Functions to Add/Subtract Amounts to Combat Attribute Stats
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddMeleeAttackPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddRangedAttackPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddSpellPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddMeleeHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddRangedHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddSpellHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddMeleeCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddRangedCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddSpellCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddArmorPenetration(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddSpellPenetration(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddDodgeChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddParryChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddBlockChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddBlockAmount(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddHaste(float Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractMeleeAttackPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractRangedAttackPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractSpellPower(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractMeleeHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractRangedHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractSpellHitChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractMeleeCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractRangedCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractSpellCriticalChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractArmorPenetration(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractSpellPenetration(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractDodgeChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractParryChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractBlockChance(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractBlockAmount(float Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractHaste(float Amount);
+
+	//Functions to Add/Subtract to Resistances
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddFireResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddFrostResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddHolyResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddShadowResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddNatureResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void AddArcaneResistance(int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractFireResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractFrostResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractHolyResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractShadowResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractNatureResistance(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void SubtractArcaneResistance(int32 Amount);
+
+	//Functions to Add/Subtract to Misc Stats
+	UFUNCTION(BlueprintCallable, Category = "Misc Stats")
+		void AddDevotion(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Misc Stats")
+		void AddProficiency(int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Misc Stats")
+		void SubtractDevotion(int32 Amount);
+	UFUNCTION(BlueprintCallable, Category = "Misc Stats")
+		void SubtractProficiency(int32 Amount);
+
+	UFUNCTION(BlueprintCallable, Category = "Combat Stats")
+		void CalculateIncomingDamage(float Amount);
+	
+};
