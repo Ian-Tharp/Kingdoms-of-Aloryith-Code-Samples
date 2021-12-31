ed48a5fe1b8ed6c790e5dfe6bfe81c58304fdc2f
Ian Tharp <64619437+Ian-Tharp@users.noreply.github.com>
2021-12-31 15:05:49 -0600

    Small Combat Functions Added

--- AC_PlayerAttributes.cpp
+++ AC_PlayerAttributes.cpp
@@ -29,7 +29,7 @@ void UAC_PlayerAttributes::TickComponent(float DeltaTime, ELevelTick TickType, F
 	// ...
 }
 
-//-------------------------------------------------------------------------------
+
 //Adding Main Player Attributes
 void UAC_PlayerAttributes::AddStrength(int32 Amount) {
 	Strength += Amount;
@@ -39,6 +39,7 @@ void UAC_PlayerAttributes::AddStrength(int32 Amount) {
 	ArmorPenetration += Amount * 0.01f;
 	ParryChance += Amount / 200.0f;
 	BlockChance += Amount / 200.0f;
+	BlockAmount += Amount;
 }
 void UAC_PlayerAttributes::AddAgility(int32 Amount) {
 	Agility += Amount;
@@ -85,7 +86,7 @@ void UAC_PlayerAttributes::AddAllMainStats(int32 Amount) {
 	AddWisdom(Amount);
 	AddCharisma(Amount);
 }
-//-------------------------------------------------------------------------------
+
 //Subtracting Main Player Attributes
 void UAC_PlayerAttributes::SubtractStrength(int32 Amount) {
 	Strength -= Amount;
@@ -94,6 +95,7 @@ void UAC_PlayerAttributes::SubtractStrength(int32 Amount) {
 	MeleeCriticalChance -= Amount * 0.01f;
 	ParryChance -= Amount / 200.0f;
 	BlockChance -= Amount / 200.0f;
+	BlockAmount -= Amount;
 }
 void UAC_PlayerAttributes::SubtractAgility(int32 Amount) {
 	Agility -= Amount;
@@ -140,7 +142,7 @@ void UAC_PlayerAttributes::SubtractAllMainStats(int32 Amount) {
 	SubtractWisdom(Amount);
 	SubtractCharisma(Amount);
 }
-//-------------------------------------------------------------------------------
+
 //Adding Combat Attributes
 void UAC_PlayerAttributes::AddMeleeAttackPower(int32 Amount) { MeleeAttackPower += Amount; }
 void UAC_PlayerAttributes::AddRangedAttackPower(int32 Amount) { RangedAttackPower += Amount; }
@@ -175,7 +177,6 @@ void UAC_PlayerAttributes::SubtractParryChance(float Amount) { ParryChance -= Am
 void UAC_PlayerAttributes::SubtractBlockChance(float Amount) { BlockChance -= Amount; }
 void UAC_PlayerAttributes::SubtractBlockAmount(float Amount) { BlockAmount -= Amount; }
 void UAC_PlayerAttributes::SubtractHaste(float Amount) { Haste -= Amount; }
-//-------------------------------------------------------------------------------
 //Adding Resistances
 void UAC_PlayerAttributes::AddFireResistance(int32 Amount) { FireResistance += Amount; }
 void UAC_PlayerAttributes::AddFrostResistance(int32 Amount) { FrostResistance += Amount; }
@@ -190,13 +191,13 @@ void UAC_PlayerAttributes::SubtractHolyResistance(int32 Amount) { HolyResistance
 void UAC_PlayerAttributes::SubtractShadowResistance(int32 Amount) { ShadowResistance -= Amount; }
 void UAC_PlayerAttributes::SubtractNatureResistance(int32 Amount) { NatureResistance -= Amount; }
 void UAC_PlayerAttributes::SubtractArcaneResistance(int32 Amount) { ArcaneResistance -= Amount; }
-//-------------------------------------------------------------------------------
+
 //Adding/Subtracting Misc Attributes
 void UAC_PlayerAttributes::AddDevotion(int32 Amount) { Devotion += Amount; }
 void UAC_PlayerAttributes::AddProficiency(int32 Amount) { Proficiency += Amount; }
 void UAC_PlayerAttributes::SubtractDevotion(int32 Amount) { Devotion -= Amount; }
 void UAC_PlayerAttributes::SubtractProficiency(int32 Amount) { Proficiency -= Amount; }
-//-------------------------------------------------------------------------------
+
 //Player Health Functions
 void UAC_PlayerAttributes::AddCurrentHealth(int32 Amount) { 
 	CurrentHealth += Amount; 
@@ -218,31 +219,228 @@ void UAC_PlayerAttributes::SubtractCurrentHealth(int32 Amount) {
 		*/
 	}
 }
-void UAC_PlayerAttributes::IncreaseMaximumHealth(int32 Amount) {
+void UAC_PlayerAttributes::AddMaximumHealth(int32 Amount) {
 	CurrentHealth += Amount;
 	MaxHealth += Amount;
 }
 
-void UAC_PlayerAttributes::DecreaseMaximumHealth(int32 Amount) {
-	if (CurrentHealth - Amount >= 1 && MaxHealth - Amount >= 1) {
+void UAC_PlayerAttributes::SubtractMaximumHealth(int32 Amount) {
+	if (CurrentHealth - Amount >= 1) {
 		CurrentHealth -= Amount;
 		MaxHealth -= Amount;
 	}
 	else {
-		CurrentHealth = 1;
-		MaxHealth = 1;
+		//Do something here
 	}
 }
-//-------------------------------------------------------------------------------
-//Take Melee Damage
-void UAC_PlayerAttributes::CalculateIncomingMeleeDamage(float Amount) {
-	float tempAmount = Amount;
-	float tempArmorCalculation = (Armor / 10) * 0.5f;
-	tempAmount = tempAmount - tempArmorCalculation;
-	if (tempAmount > 1) {
-		SubtractCurrentHealth(tempAmount);
+
+//-------------------------------------------------------------------------------------------------------------------
+//Hit Chance Functions
+bool UAC_PlayerAttributes::DetermineSuccessful_MeleeHit() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance > MeleeHitChance) {
+		//If random number is greater than the hit chance, return false, the player misses the attack
+		return false;
 	}
 	else {
-		SubtractCurrentHealth(1);
+		return true;
+	}
+}
+bool UAC_PlayerAttributes::DetermineSuccessful_RangedHit() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance > RangedHitChance) {
+		//If random number is greater than the hit chance, return false, the player misses the attack
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+bool UAC_PlayerAttributes::DetermineSuccessful_SpellHit() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance > SpellHitChance) {
+		//If random number is greater than the hit chance, return false, the player misses the attack
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+bool UAC_PlayerAttributes::DetermineParry() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance < ParryChance) {
+		//If random number is less than the parry chance (due to the low number nature), return true for successful parry on attack
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool UAC_PlayerAttributes::DetermineDodge() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance < DodgeChance) {
+		//If random number is less than the dodge chance (due to the low number nature), return true for successful dodge
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool UAC_PlayerAttributes::DetermineBlock() {
+	tempChance = rand() % 100 + 1;
+	if (tempChance < BlockChance) {
+		//If random number is less than the block chance (due to the low number nature), return true for successful block
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//Take Melee Damage
+int32 UAC_PlayerAttributes::TakeIncomingMeleeDamage(int32 Amount) {
+	if (DetermineParry() == true) {
+		return 0;
+	}
+	if (DetermineDodge() == true) {
+		return 0;
+	}
+	if (DetermineBlock() == true) {
+		float tempAmount = Amount;
+		float tempArmorCalculation = (Armor / 10) * 0.5f;
+		tempAmount = tempAmount - tempArmorCalculation;
+		tempAmount = tempAmount - BlockAmount;
+		tempAmount = ceil(tempAmount);
+		if (tempAmount >= 0) {
+			SubtractCurrentHealth(tempAmount);
+		}
+		else {
+			SubtractCurrentHealth(0);
+		}
+		return tempAmount;
+	}
+	else {
+		float tempAmount = Amount;
+		float tempArmorCalculation = (Armor / 10) * 0.5f;
+		tempAmount = tempAmount - tempArmorCalculation;
+		tempAmount = ceil(tempAmount);
+		if (tempAmount > 1) {
+			SubtractCurrentHealth(tempAmount);
+		}
+		else {
+			SubtractCurrentHealth(1);
+		}
+		return tempAmount;
+	}
+}
+
+//Take Ranged Damage
+int32 UAC_PlayerAttributes::TakeIncomingRangedDamage(int32 Amount) {
+	if (DetermineDodge() == true) {
+		return 0;
+	}
+	else {
+		float tempAmount = Amount;
+		float tempArmorCalculation = (Armor / 10) * 0.5f;
+		tempAmount = tempAmount - tempArmorCalculation;
+		tempAmount = ceil(tempAmount);
+		if (tempAmount > 1) {
+			SubtractCurrentHealth(tempAmount);
+		}
+		else {
+			SubtractCurrentHealth(1);
+		}
+		return tempAmount;
+	}
+}
+
+//Take Spell Damage
+int32 UAC_PlayerAttributes::TakeIncomingSpellDamage(int32 Amount, int MagicType) {
+	if (DetermineDodge() == true) {
+		return 0;
+	}
+	else {
+		float tempAmount = Amount;
+		float tempArmorCalculation = (Armor / 100) * 0.5f;
+		tempAmount = tempAmount - tempArmorCalculation;
+		switch (MagicType) {
+		//Fire Damage
+		case 1:
+			tempAmount = tempAmount - FireResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Frost Damage
+		case 2:
+			tempAmount = tempAmount - FrostResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Holy Damage
+		case 3:
+			tempAmount = tempAmount - HolyResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Shadow Damage
+		case 4:
+			tempAmount = tempAmount - ShadowResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Nature Damage
+		case 5:
+			tempAmount = tempAmount - NatureResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Arcane Damage
+		case 6:
+			tempAmount = tempAmount - ArcaneResistance;
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		//Other Spell Damage (non resistant)
+		default:
+			if (tempAmount > 1) {
+				SubtractCurrentHealth(tempAmount);
+			}
+			else {
+				SubtractCurrentHealth(1);
+			}
+			return tempAmount;
+			break;
+		}
 	}
 }
